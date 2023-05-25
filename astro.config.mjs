0200db85e2996e540dc5120b639d4647d6789c0a
youns <youns.s.ibrahim115@gmail.com>
2023-05-25 22:11:40 +0300

    import image fn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import image from '@astrojs/image';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+
+	integrations: [image(), mdx(), sitemap()],
 });
